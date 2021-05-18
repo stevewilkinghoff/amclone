708113bc972eb8746792ab3407aacce7363c415a
stevewilkinghoff <stevewilkinghoff@gmail.com>
2021-05-18 05:40:39 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+
+  const firstName = "Steve"
+  return (
+    <div className="App">
+      <Header title="Amazon Cart"/>
+    </div>
+  );
+}
+
+export default App;
